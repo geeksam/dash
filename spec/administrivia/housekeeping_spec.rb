7560b1a5ea6361f887aeb6bfde95e31b033de8ba
Sam Livingston-Gray <sam@renewfund.com>
2011-03-25 10:45:43 -0700

    Specs now remind me to drop generated crap.

---
+++ spec/administrivia/housekeeping_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe "directory structure" do
+  ## These specs exist to keep me from checking in useless crap from the rails generators.
+  
+  # Using RSpec, not Test::Unit 
+  it "should not have a /test directory" do
+    Dir.entries(Rails.root).should_not include('test')
+  end
+  
+  # Using /spec/integration in place of /spec/requests
+  it "should not have a /spec/requests directory" do
+    Dir.entries(File.join(Rails.root, *%w[spec])).should_not include('requests')
+  end
+
+  # Helpers!  We don't need 'em!  (Delete these specs if, at some point, we do.)
+  it "should not have controller-specific helpers" do
+    Dir.entries(File.join(Rails.root, *%w[app helpers])).should == %w[. .. application_helper.rb]
+  end
+  it "should not have a spec/helpers directory (until we actually need one!)" do
+    Dir.entries(File.join(Rails.root, *%w[spec])).should_not include('helpers')
+  end
+end
